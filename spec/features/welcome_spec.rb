a06ded224e341e9fd32431464fd49f971ae1409f
Nathanael Burt <nathanael.burt@gmail.com>
2014-09-11 21:35:52 -0600

    delete unnecessary code

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
 feature 'visiting the homepage' do
-  scenario 'a user sees basic information on the homepage' do
-    visit '/'
-    expect(page).to have_content 'Hunan Springs'
-    expect(page).to have_content 'Menu'
-    expect(page).to have_content 'About Us'
-    expect(page).to have_content 'Contact Us'
-  end
-
   scenario 'a user can visit the menu page' do
     create_categories
     create_menu_types
